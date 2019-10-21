1103d53c649256a1861443de692ffcf6195c7055
Junya-kobayashi <atsubeee@doi.ics.keio.ac.jp>
2019-10-21 14:41:22 +0900

    get product

--- Challenge-1-Azure-Function-Basics/GetProduct/index.js
+++ Challenge-1-Azure-Function-Basics/GetProduct/index.js
@@ -1,10 +1,11 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, req, productDocument) {
     context.log('JavaScript HTTP trigger function processed a request.')
 
-    if (req.query.productId ) {
+    if (req.query.productId) {
+        context.log(productDocument)
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Starfruit Explosion " + req.query.productId
+            body: "Starfruit Explosion " + productDocument
         };
     }
     else {
